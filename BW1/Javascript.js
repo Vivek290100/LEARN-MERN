9cfd12c5ae1e54e70d74ef23d8dd6c42970d57c7
vivek <vivekv290100@gmail.com>
2025-06-14 19:37:35 +0530

    BW1-4

--- BW1/Javascript.js
+++ BW1/Javascript.js
@@ -91,7 +91,7 @@
 // for (let i = 0; i < ba.length; i++) {
 //   sum+=ba[i].a
 //   sum+=ba[i].adf.l.reduce((acc,curr)=>acc+curr)
-  
+
 // }
 
 // console.log(sum);
@@ -112,7 +112,7 @@
 // call apply bind
 // function name(a,b){
 //   console.log(this.name, a,b);
-  
+
 // }
 
 // const obj = {
@@ -163,7 +163,7 @@
 
 //   greet(){
 //     console.log(this.name);
-    
+
 //   }
 // }
 
@@ -238,7 +238,7 @@
 //    resolve(20)
 // }).then((result)=>{
 //   console.log(a+result);
-  
+
 // })
 
 // ======================
@@ -262,26 +262,235 @@
 // }).then((result)=>{
 //   console.log(result);
 //   console.log("completed");
-  
+
 // }).catch((error)=>{
 //   console.log(error);
-  
+
 // })
 
 
 // ===============================================
-function findLargeSubstring(str){
-  let ans = ""
-  for (let i = 0; i < str.length; i++) {
-    let string = ""
-    while(str[i]==str[i+1]){
-      string+=str[i]
-      i++
-    }
-    if(string.length>ans.length){
-      ans = string+str[i]
-    }
+// function findLargeSubstring(str){
+//   let ans = ""
+//   for (let i = 0; i < str.length; i++) {
+//     let string = ""
+//     while(str[i]==str[i+1]){
+//       string+=str[i]
+//       i++
+//     }
+//     if(string.length>ans.length){
+//       ans = string+str[i]
+//     }
+//   }
+//   return ans
+// }
+// console.log(findLargeSubstring("qweeeeeertyyyyyyyyuuu"));
+
+
+// ==================================================
+// const nestedArray = [
+//   1,
+//   [2, 3],
+//   [4, [5, 6]],
+//   [7, [8, [9, 10]]]
+// ];
+
+// function fun(arr){
+//   let result = []
+//     for (const element of arr) {
+//       if(Array.isArray(element)){
+//         result = result.concat(fun(element))
+//       }else{
+//         result.push(element)
+//       }
+//     }
+
+//   return result
+// }
+
+// console.log(fun(nestedArray))
+
+// ==========================================
+// const arr = [3,3,2,2,2,3,4,5,5,6,7,7,8,8]
+// function fun(arr){
+//   let result = []
+//   for (let i = 0; i < arr.length; i++) {
+//     let duplicate = false
+//     for (let j = 0; j < result.length; j++) {
+//       if(arr[i]==result[j]){
+//         duplicate = true
+//         break
+//       }
+//     }
+//     if(!duplicate){
+//       result.push(arr[i])
+//     }
+
+//   }
+//   return result
+// }
+// console.log(fun(arr));
+
+// =============================================
+// const arr = [3, 3, 0, 0, 7, 0, 7, 8, 8]
+// function fun(arr) {
+//   let ind = 0
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== 0) {
+//       arr[ind] = arr[i]
+//       ind++
+//     }
+//   }
+//   for (let i = ind; i < arr.length; i++) {
+//     arr[i] = 0
+//   }
+//   return arr
+
+// }
+
+// console.log(fun(arr));
+
+// ==============================================
+
+// const parantheses = "[{({[]})}]"
+// const result = []
+// function fun(parantheses) {
+//   for (const i of parantheses) {
+//     if (i == "{") {
+//       result.push("}")
+//     } else if (i == "[") {
+//       result.push("]")
+//     } else if (i == "(") {
+//       result.push(")")
+//     } else {
+//       if(result.pop()!==i){
+//         return false
+//       }
+//     }
+//   }
+//   return result.length == 0
+// }
+
+// console.log(fun(parantheses));
+
+// ================================
+// function palindrome(string){
+//   let start = 0
+//   let end = string.length-1
+//   while(start<end){
+//     if (string[start] !== string[end]){
+//       return false
+//     }
+//     start++
+//     end--
+//   }
+//   return true
+// }
+
+// function longestSubStr(str){
+//   let longest = ""
+//   for (let i = 0; i < str.length; i++) {
+//     for (let j = i+1; j <= str.length; j++) {
+//       const substring = str.slice(i,j)
+//       if(palindrome(substring) && substring.length > longest.length){
+//         longest = substring
+//       }
+//     }
+    
+//   }
+//   return longest
+// }
+
+// console.log(longestSubStr("dsasabccbaasd"));
+
+// =====================================
+// const arr = [2,3,4,5,6,7]
+// function fun(arr, i=0){
+//   if(i>=arr.length){
+//     return 0
+//   }
+//   if(even(arr[i])){
+//     return arr[i] + fun(arr, i + 1)
+//   }else{
+//     return fun(arr,i+1)
+//   }
+// }
+// function even(a){
+//   return a%2==0
+// }
+
+// console.log(fun(arr));
+
+// class HashTable{
+//   constructor(size){
+//     this.table = new Array(size)
+//   }
+
+//   hash(key){
+//     let hash = 0
+//     for (let i = 0; i < key.length; i++) {
+//       hash+=key.charCodeAt(i)
+//     }
+//     return hash%this.table.length
+//   }
+
+//   // set(key, value){
+//   //   const index = this.hash(key)
+//   //   this.table[index] = value
+//   // }
+
+//   set(key,value){
+//     const index = this.hash(key)
+//     if(!this.table[index]){
+//       this.table[index] = []
+//     }
+//     this.table[index].push(key,value)
+//   }
+
+//   get(key){
+//     const index = this.hash(key)
+//     return this.table[index]
+//   }
+  
+//   remove(key){
+//     const index = this.hash(key)
+//     this.table[index] = undefined
+//   }
+
+//   print(){
+//     for (let i = 0; i < this.table.length; i++) {
+//       if(this.table[i]){
+//         console.log(i, this.table[i]);
+        
+//       }
+//     }
+//   }
+
+// }
+
+// const hashTable = new HashTable(10)
+// hashTable.set("javascript", 123)
+// hashTable.set("mongodb", 12345)
+// hashTable.set("mongodb-database", 1234567)
+
+// hashTable.print()
+// // console.log(hashTable);
+
+class Stack{
+  constructor(){
+    this.items = []
   }
-  return ans
+
+  push(value){
+    return this.items.push(value)
+  }
+
+  pop(){
+    return this.items.pop()
+  }
+
+  reverse(string){
+    const newStack = new Stack()
+    
+  }
 }
-console.log(findLargeSubstring("qweeeeeertyyyyyyyyuuu"));
--- Javascript/logicQuestions/longestPalindromicSubstring.js
+++ Javascript/logicQuestions/longestPalindromicSubstring.js
@@ -1,12 +1,29 @@
-let s = "babad";
-function fun(s) {
-    for (let i = 0; i < s.length; i++) {
-        for (let j = s.length-1; j >=0; j--) {
-            
-        }
 
+function palindrome(string){
+  let start = 0
+  let end = string.length-1
+  while(start<end){
+    if (string[start] !== string[end]){
+      return false
     }
+    start++
+    end--
+  }
+  return true
 }
 
-console.log(fun(s));
+function longestSubStr(str){
+  let longest = ""
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i+1; j <= str.length; j++) {
+      const substring = str.slice(i,j)
+      if(palindrome(substring) && substring.length > longest.length){
+        longest = substring
+      }
+    }
+    
+  }
+  return longest
+}
 
+console.log(longestSubStr("dsasabccbaasd"));
