94528f41ed48733695373b6a89f83a781ce2d2df
vivek <vivekv290100@gmail.com>
2025-07-08 20:01:52 +0530

    4

---
+++ BW2/node.js
@@ -0,0 +1,19 @@
+// const express = require("express")
+
+// const app = express()
+
+
+// app.listen(3000,() =>{
+//     console.log("http://localhost:4000");
+// })
+
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Node.js!');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
