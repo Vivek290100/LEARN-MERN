50a6e0af2482950c4c5e69ae8ef08c6fa576f3c8
vivek <vivekv290100@gmail.com>
2025-08-16 13:24:39 +0530

    21

--- BW2/js.js
+++ BW2/js.js
@@ -1022,3 +1022,37 @@
 // db.customers.aggregate([{$lookup: {from: 'orders',localField: '_id',foreignField: 'customerId',as: 'customerOrders'}}]);
 
 
+
+
+// function heapSort(arr) {
+//     let n = arr.length;
+
+//     // Step 1: Build Max Heap
+//     for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+//         heapify(arr, n, i);
+//     }
+
+//     // Step 2: Extract elements from heap
+//     for (let i = n - 1; i > 0; i--) {
+//         [arr[0], arr[i]] = [arr[i], arr[0]]; // Move max to end
+//         heapify(arr, i, 0); // Heapify remaining
+//     }
+
+//     return arr;
+// }
+
+// function heapify(arr, n, i) {
+//     let largest = i;
+//     let left = 2 * i + 1;
+//     let right = 2 * i + 2;
+
+//     if (left < n && arr[left] > arr[largest]) largest = left;
+//     if (right < n && arr[right] > arr[largest]) largest = right;
+
+//     if (largest !== i) {
+//         [arr[i], arr[largest]] = [arr[largest], arr[i]];
+//         heapify(arr, n, largest);
+//     }
+// }
+
+// console.log("sort",heapSort([3,5,6,7,4,1,9,6,0,3]));
