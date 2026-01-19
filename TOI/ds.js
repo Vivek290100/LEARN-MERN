59ced3d9b9b5223d8043232aced8ca872d4e3d6d
vivek <vivekv290100@gmail.com>
2026-01-19 21:19:31 +0530

    2

--- TOI/ds.js
+++ TOI/ds.js
@@ -20,5 +20,27 @@ class Graph{
         this.adjacencyList[vertex2].push(vertex1)
     }
 
-    bfs(start)
+    bfs(start){
+        
+    }
+
+      detectCycle(start){
+    const visited = {}
+    const dfs = (vertex, parent)=>{
+      visited[vertex] = true
+      for(const neighbor of this.adjacancyList[vertex]){
+        if(!visited[neighbor]){
+          if(dfs(neighbor, vertex)){
+            return true
+          }
+        }else{
+          if(neighbor!==parent){
+            return true
+          }
+        }
+      }
+      return false
+    }
+    return dfs(start, null)
+  }
 }
